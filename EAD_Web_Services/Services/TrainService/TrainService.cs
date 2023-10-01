aba99359b2ea0e83e9da85e8ea1e8b03a74d2ddc
Piumika Saranga <IT20260460@my.sliit.lk>
2023-10-02 02:11:02 +0530

    clean the service code

--- EAD_Web_Services/Services/TrainService/TrainService.cs
+++ EAD_Web_Services/Services/TrainService/TrainService.cs
@@ -40,7 +40,8 @@ namespace EAD_Web_Services.Services.TrainService
         {
             List<TrainsResponseBody> filteredTrains = new();
 
-            // Call function to filter trains by departure and arrival. //todo: check train is active
+            // Call function to filter trains by departure and arrival. 
+            //TODO: check if train is active
             var trains = FilterTrainsByDepartureAndArrival(trainsRequestBody.Departure, trainsRequestBody.Arrival);
 
            
@@ -55,34 +56,15 @@ namespace EAD_Web_Services.Services.TrainService
                 if (availableSeats >= trainsRequestBody.SeatCount)
                 {
 
-                 
                     // Find the departure station
                     var departureStation = train.Stations?.FirstOrDefault(station => station.StationName == trainsRequestBody.Departure);
 
                     // Find the arrival station
                     var arrivalStation = train.Stations?.FirstOrDefault(station => station.StationName == trainsRequestBody.Arrival);
 
-                    //calculate the price based on the train type ,number of stations and the time between the departure and arrival
-                    var trainTypeCharge = train.TrainTypesDetails.Price;
-                    //get the number of stations between the departure and arrival including the departure and arrival stations
-                    var numberOfStations = train.Stations?.Count(station => station.StationName == trainsRequestBody.Departure ||
-                                                                                               station.StationName == trainsRequestBody.Arrival);
-                    //console log the number of stations
-                    Console.WriteLine($"number of stations :: {numberOfStations}");
-
-                    //get the time between the departure and arrival
-                    var timeBetweenDepartureAndArrival = arrivalStation?.Time - departureStation?.Time;
-
-                    //console log the time between the departure and arrival
-                    Console.WriteLine($"time between departure and arrival :: {timeBetweenDepartureAndArrival?.Hours}");
-
-                    //calculate the price more real world scenario
-                    var price = trainTypeCharge *( (numberOfStations ?? 0) + (timeBetweenDepartureAndArrival?.TotalHours ?? 0));
-
-                    //console log the price
-                    Console.WriteLine($"price :: {price}");
-
-
+                    //calculate price
+                    var price = CalculatePrice(train, trainsRequestBody.Departure, trainsRequestBody.Arrival, departureStation, arrivalStation);
+  
                     //add train to filtered trains
                     filteredTrains.Add(new TrainsResponseBody
                     {
@@ -92,7 +74,7 @@ namespace EAD_Web_Services.Services.TrainService
                         Arrival = trainsRequestBody.Arrival,
                         DepartureTime = departureStation?.Time ?? DateTime.MinValue,
                         ArrivalTime = arrivalStation?.Time ?? DateTime.MinValue,
-                        TripTimeDuration = timeBetweenDepartureAndArrival?.TotalHours ?? 0,
+                        TripTimeDuration = (arrivalStation?.Time - departureStation?.Time)?.TotalHours ?? 0,
                         AvailableSeatCount = availableSeats,
                         RequestedSeatCount = trainsRequestBody.SeatCount,
                         Amount = price
@@ -104,6 +86,20 @@ namespace EAD_Web_Services.Services.TrainService
             }
 
             return filteredTrains;
+        }
+
+        //calculate price
+        public double CalculatePrice(Train train, string departure, string arrival , Station departureStation , Station arrivalStation)
+        {
+           
+            var trainTypeCharge = train.TrainTypesDetails.Price;
+            var numberOfStations = train.Stations?.Count(station => station.StationName == departure || station.StationName == arrival);
+
+            var timeBetweenDepartureAndArrival = arrivalStation?.Time - departureStation?.Time;
+
+            var price = trainTypeCharge * ((numberOfStations ?? 0) + (timeBetweenDepartureAndArrival?.TotalHours ?? 0));
+
+            return price;
         }
 
         //get available seats
@@ -130,17 +126,14 @@ namespace EAD_Web_Services.Services.TrainService
                 train.Stations.Any(station => station.StationName == departure) &&
                 train.Stations.Any(station => station.StationName == arrival) &&
                 train.Stations.Any(station => station.StationName == departure &&
-                                              station.Time < train.Stations.First(s => s.StationName == arrival).Time);
+                                              station.Time < train.Stations.First(s => s.StationName == arrival).Time) &&
+                train.IsActive; // Check if the train is active
 
             // Apply the combined filter using the Where method.
             var filteredTrains = _trains.AsQueryable().Where(combinedFilter).ToList();
 
             return filteredTrains;
         }
-
-
-
-
 
         public void Remove(string id)
         {
