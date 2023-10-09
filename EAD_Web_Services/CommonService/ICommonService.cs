d608900daa833f1b128431b6b280f7f8f2b9490d
Devsrini Savidya <savidyasamaranayake@gmail.com>
2023-10-09 18:02:06 +0530

    Add comment for the common services

--- EAD_Web_Services/CommonService/ICommonService.cs
+++ EAD_Web_Services/CommonService/ICommonService.cs
@@ -1,8 +1,18 @@
-﻿using EAD_Web_Services.Models.ReservationModel;
+﻿//   Sri Lanka Institute of Information Technology
+//   Year  :  4th Year 2nd Semester
+//   Module Code  :  SE4040
+//   Module  :  Enterprise Application Development
+//   Student Id Number  :  IT20260460
+//   Name  :  Piumika Saranga H.A.
+
+using EAD_Web_Services.Models.ReservationModel;
 using EAD_Web_Services.Models.TrainModel;
 
 namespace EAD_Web_Services.CommonService
 {
+    /// <summary>
+    /// / Common service interface for handling train-related operations.
+    /// </summary>
     public interface ICommonService
     {
         List<Reservation> GetByTrainIdAndDate(string trainId, DateTime date);
